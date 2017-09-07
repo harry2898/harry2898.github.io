241414228e5a8460fd0f9415ae00d99dcda1dd14
harry2898 <31583354+harry2898@users.noreply.github.com>
2017-09-07 11:52:31 -0500

    Updated Com S 227 Labs
    ^

--- Com S 227/cs227/workspace/project2/src/lab2/Atom.java
+++ Com S 227/cs227/workspace/project2/src/lab2/Atom.java
@@ -1,5 +1,30 @@
 package lab2;
 
-public class Atom {
 
+public class Atom
+{
+	private int givenProtons;
+
+	private int givenNeutrons;
+	
+	private int givenElectrons;
+	
+	
+	public int getAtomicMass()
+	{
+		int AtomicMass = givenProtons + givenNeutrons;
+		return AtomicMass;
+	}
+
+	public int getAtomicCharge()
+	{
+		int AtomicCharge = givenProtons - givenElectrons;
+		return AtomicCharge;
+	}
+	public void decay()
+	{
+		int givenProtons = givenProtons - 2;
+		int givenNeutrons = givenNeutrons - 2;
+	}
 }
+
--- Com S 227/cs227/workspace/project2/src/lab2/AtomTest.java
+++ Com S 227/cs227/workspace/project2/src/lab2/AtomTest.java
@@ -1,5 +1,8 @@
 package lab2;
 
 public class AtomTest {
-
+	public static void main(String[] args){
+		Atom ;
+		
+	}
 }
