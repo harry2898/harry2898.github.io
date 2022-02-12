46af3508f228d7266f9c982ea790465c75d06e96
harry2898 <2016hhelgevold@gmail.com>
2022-02-12 14:24:16 -0600

    Tileset Stuff and Misc

---
+++ repo_contents/tilesets/misc/test_paths_tiled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="test_paths_tiled" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="400" columns="20">
+ <image source="test_paths_tileset.png" width="661" height="661"/>
+</tileset>
